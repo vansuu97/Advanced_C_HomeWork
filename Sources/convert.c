f4cfd71fca80cbf35339585cd21e2fcfefb116c8
Vansuu97 <vansuu97.138@gmail.com>
2022-05-08 02:42:03 +0700

    commit lan 5

---
+++ Sources/convert.c
@@ -0,0 +1,21 @@
+#include "..\Header\convert.h"
+
+
+void Dec_to_Bin(int M)
+{
+    printf("\n ==>So DECIMA: %d chuyen sang BINARY co gia tri la: ", M);
+    // Size cua bien integer co 4byte = 32 bit
+    int i=0, k=0, x=0;
+    while (i<32)
+        {  
+            k = M >> i; 
+            if ((k|0)==0) break;
+            x = x << 1;
+            if (k & 1)  {x= x|1;} 
+            i++;
+        } 
+    for (k=0; k<i; k++)
+        if (((x>>k) & 1) == 1) printf("1");
+            else printf("0");
+
+}
--- Sources/main.c
+++ Sources/main.c
@@ -5,8 +5,11 @@
 
 int main()
     {
-        int X=10;
+        int X=156;
         printf("HomeWork_Session_2");
-        //Dec_to_Bin(X);
+        Dec_to_Bin(X);
         return 0;
     }
+
+
+//gcc Sources/main.c Sources/calc.c Sources/convert.c Sources/m_string.c Header/calc.h Header/convert.h Header/m_string.h -o susu
